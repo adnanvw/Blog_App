6c91c9800505c0698d7ed39ca0d443b998089071
adnanvw <adnan1.vw@gmail.com>
2023-10-27 18:05:24 +0530

    blog app

---
+++ README.md
@@ -0,0 +1,53 @@
+# Blog App using MERN Stack
+
+Welcome to the Blog App repository! This application is built using the MERN (MongoDB, Express, React, Node.js) stack and comes with various functionalities to manage and share your blogs.
+
+
+## Functionalities
+
+- **Authentication:** Secure user authentication system to protect your blogs.
+- **Create Blog:** Easily create and publish your blogs with a user-friendly interface.
+- **Delete Blog:** Remove unwanted blogs with a simple delete option.
+- **Update Blog:** Edit and update your blogs as your content evolves.
+- **View Other User Blogs:** Explore and read blogs published by other users.
+
+
+## Getting Started
+
+To get started with this project, follow these steps:
+
+1. Fork this repository
+
+1. Clone this repository to your local machine:
+
+```bash
+git clone https://github.com/adnanvw/Blog_App.git
+```
+
+2. Install the required dependencies for both the backend and frontend:
+
+```bash
+cd Blog-App-using-MERN-stack
+cd server && npm install
+cd ../client && npm install
+```
+
+3. Configure the database connection in the backend. You can use MongoDB Atlas or a local MongoDB server.
+
+4. Start the backend server:
+
+```bash
+cd server && npm start
+```
+
+5. Start the frontend application:
+
+```bash
+cd client && npm start
+```
+
+
+6. Access the application in your web browser at [http://localhost:3000](http://localhost:3000).
+
+
+Happy blogging!
---
+++ client/src/componets/Login.js
@@ -0,0 +1,135 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const naviagte = useNavigate();
+  const dispath = useDispatch();
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignup, setIsSignup] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const sendRequest = async (type = "login") => {
+    try {
+      const res = await axios.post(`http://localhost:5000/api/users/${type}`, {
+        name: inputs.name,
+        email: inputs.email,
+        password: inputs.password,
+      });
+
+      if (res.status >= 200 && res.status < 400) {
+        const data = res.data;
+        console.log("return");
+        console.log(data);
+        return data;
+      } else {
+        return null;
+      }
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    if (isSignup) {
+      const data = await sendRequest("signup")
+      if (!data) {
+        alert("Something went wrong...")
+        return
+      } else {
+        localStorage.setItem("userId", data?.user._id)
+        dispath(authActions.login())
+        naviagte("/blogs")
+      }
+    } else {
+
+      const data = await sendRequest()
+      if (!data) {
+        alert("Something went wrong...")
+        return
+      } else {
+        localStorage.setItem("userId", data?.user._id)
+        dispath(authActions.login())
+        naviagte("/blogs")
+      }
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          maxWidth={400}
+          display="flex"
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={5}
+        >
+          <Typography variant="h2" padding={3} textAlign="center">
+            {isSignup ? "Signup" : "Login"}
+          </Typography>
+          {isSignup && (
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              placeholder="Name"
+              margin="normal"
+            />
+          )}{" "}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Change To {isSignup ? "Login" : "Signup"}
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
